a334cc12fba005fcd8fdc41e1b8e29b27404f193
NazliQ <nazlipikulser@gmail.com>
2024-09-11 15:20:41 +0200

    Fjernet svarData json fil, da dette legges ved i data.js

--- MakerspaceSurvivaleGuide/src/data/data.js
+++ MakerspaceSurvivaleGuide/src/data/data.js
@@ -3,14 +3,18 @@ export const cardData = [
         "id": 1,
         "title": "What is a Makerspace?",
         "utitle": "Collaborative workspace⁠, tools, technology, hands-on learning, innovation",
+        "svar": `A Makerspace is a collaborative workspace equipped with tools and technology for creating,learning, 
+                exploring, and sharing ideas. It typically offers resources like 3D printers, electronics, and other fabrication tools to foster innovation and hands-on learning`, 
         "kategori": "yellow",
     },  
     {
         "id": 2, 
         "title": "What can you make at Makerspace?",
         "utitle": "Class project, personal project, prototype, physical products, digital products",
-        "svar": `A Makerspace is a collaborative workspace equipped with tools and technology for creating,learning, 
-                exploring, and sharing ideas. It typically offers resources like 3D printers, electronics, and other fabrication tools to foster innovation and hands-on learning`,
+        "svar": `As a student at OsloMet, you can use the Makerspace to bring your creative ideas to life, 
+                within the framework and tools available. Whether you want to prototype a new invention, work on a class project, 
+                or explore personal interests, the Makerspace provides the resources and environment to support your endeavors. 
+                You have the freedom to create what you desire, as long as it aligns with the Makerspace's guidelines and utilizes the available equipment.`,
         "kategori": "purple",
      
     },      
@@ -18,6 +22,12 @@ export const cardData = [
         "id": 3,
         "title": "Can I eat and drink at the Makerspace?",
         "utitle": "Safety, cleanliness, professionalism, allergy, damaged projects and equipment",
+        "svar": `No, you should not eat and drink in a Makerspace. There are several reasons for this: 
+                Safety: Makerspaces often contain sensitive electronic components, tools, and machines. Food and drinks can cause damage if spilled on the equipment.
+                Cleanliness: A clean working environment is important to maintain the functionality and lifespan of the equipment.Food remnants can attract pests or cause bacterial growth.
+                Focus and professionalism: Makerspace is primarily a work and learning environment. Avoiding food and drinks helps maintain a professional environment and keeps the focus on project work. 
+                Allergy considerations: Some people may have severe food allergies. By keeping food out of the area, the risk of allergic reactions is reduced.
+                Protection of projects: Ongoing projects of others can be damaged by spills or food residues. It is common practice to have dedicated areas outside the actual workspace for eating and drinking.`,
         "kategori": "green",
     },
     {
