57b246d7443330d48000c153ef271c757703d046
NazliQ <nazlipikulser@gmail.com>
2024-09-11 11:19:32 +0200

    Lagt til mer data i json filer

--- MakerspaceSurvivaleGuide/src/data/data.js
+++ MakerspaceSurvivaleGuide/src/data/data.js
@@ -1,30 +1,29 @@
 export const cardData = [
     {
         "id": 1,
-        "title": "Makerspace files",
-        "utitle": "Hva en bør gjøre for å finne filer",
-        "kategori": "yellow"
+        "title": "What is a Makerspace?",
+        "utitle": "Collaborative workspace⁠, tools, technology, hands-on learning, innovation",
+        "kategori": "yellow",
     },  
     {
         "id": 2, 
-        "title": "Makerspace files",
-        "utitle": "Hva en bør gjøre for å finne filer",
-        "kategori": "purple"
-
+        "title": "What can you make at Makerspace",
+        "utitle": "Class project, personal project, prototype, physical products, digital products",
+        "kategori": "purple",
+     
     },      
     {
         "id": 3,
-        "title": "Makerspace files",
-        "utitle": "Hva en bør gjøre for å finne filer",
-        "kategori": "green"
-
-
+        "title": "Can I eat and drink at the Makerspace",
+        "utitle": "Safety, cleanliness, professionalism, allergy, damaged projects and equipment",
+        "kategori": "green",
     },
     {
         "id": 4,
         "title": "Makerspace files",
         "utitle": "Hva en bør gjøre for å finne filer",
-        "kategori": "yellow"
+        "kategori": "yellow",
+     
 
 
     },
@@ -32,10 +31,102 @@ export const cardData = [
         "id": 5,
         "title": "Makerspace files",
         "utitle": "Hva en bør gjøre for å finne filer",
-        "kategori": "yellow"
+        "kategori": "yellow",
+        
 
 
     },
+    {
+        "id": 6,
+        "title": "Makerspace files",
+        "utitle": "Hva en bør gjøre for å finne filer",
+        "kategori": "yellow",
+     
+
+
+    },
+    {
+        "id": 7,
+        "title": "Makerspace files",
+        "utitle": "Hva en bør gjøre for å finne filer",
+        "kategori": "yellow",
+       
+
+
+    },
+    {
+        "id": 8,
+        "title": "Makerspace files",
+        "utitle": "Hva en bør gjøre for å finne filer",
+        "kategori": "yellow",
+      
+
+
+    },
+    {
+        "id": 9,
+        "title": "Makerspace files",
+        "utitle": "Hva en bør gjøre for å finne filer",
+        "kategori": "yellow",
+       
+
+
+    },
+    {
+        "id": 10,
+        "title": "Makerspace files",
+        "utitle": "Hva en bør gjøre for å finne filer",
+        "kategori": "yellow",
+      
+
+
+    },
+    {
+        "id": 11,
+        "title": "Makerspace files",
+        "utitle": "Hva en bør gjøre for å finne filer",
+        "kategori": "yellow",
+     
+
+
+    },
+    {
+        "id": 12,
+        "title": "Makerspace files",
+        "utitle": "Hva en bør gjøre for å finne filer",
+        "kategori": "yellow",
+
+    },
+    {
+        "id": 13,
+        "title": "Makerspace files",
+        "utitle": "Hva en bør gjøre for å finne filer",
+        "kategori": "yellow",
+
+    },
+    {
+        "id": 14,
+        "title": "Makerspace files",
+        "utitle": "Hva en bør gjøre for å finne filer",
+        "kategori": "yellow",
+
+    },
+    {
+        "id": 15,
+        "title": "Makerspace files",
+        "utitle": "Hva en bør gjøre for å finne filer",
+        "kategori": "yellow",
+
+    },
+    {
+        "id": 16,
+        "title": "Makerspace files",
+        "utitle": "Hva en bør gjøre for å finne filer",
+        "kategori": "yellow",
+
+    },
+
+
 
 
 ];
